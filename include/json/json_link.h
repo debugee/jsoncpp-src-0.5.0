5e8c2b3ba5ed8446f59954982a552ed298c7fb42
debugee <519306795@qq.com>
2023-07-25 20:03:45 +0800

    link.h

--- include/json/json_link.h
+++ include/json/json_link.h
@@ -1,8 +1,31 @@
 #pragma once
 
-
 #ifdef _DEBUG
-	#pragma comment(lib, "v12_smdd_libjson.lib")
+	#ifdef _DLL
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smdd_libjson.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smdd_libjson.lib")
+		#endif
+	#else
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smtd_libjson.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smtd_libjson.lib")
+		#endif
+	#endif
 #else
-	#pragma comment(lib, "v12_smd_libjson.lib")
+	#ifdef _DLL
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smd_libjson.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smd_libjson.lib")
+		#endif
+	#else
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smt_libjson.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smt_libjson.lib")
+		#endif
+	#endif
 #endif
