5a6ae7a156e542dfcb13d8c84f71577ad56245a6
debugee <519306795@qq.com>
2024-01-18 21:09:41 +0800

    MSVC_TOOLSET_VERSION

--- include/json/json_link.h
+++ include/json/json_link.h
@@ -2,29 +2,29 @@
 
 #ifdef _DEBUG
 	#ifdef _DLL
-		#if _MSC_VER == 1800
+		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smdd_libjson.lib")
-		#elif _MSC_VER == 1900
+		#elif MSVC_TOOLSET_VERSION == 140
 			#pragma comment(lib, "v14_smdd_libjson.lib")
 		#endif
 	#else
-		#if _MSC_VER == 1800
+		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smtd_libjson.lib")
-		#elif _MSC_VER == 1900
+		#elif MSVC_TOOLSET_VERSION == 140
 			#pragma comment(lib, "v14_smtd_libjson.lib")
 		#endif
 	#endif
 #else
 	#ifdef _DLL
-		#if _MSC_VER == 1800
+		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smd_libjson.lib")
-		#elif _MSC_VER == 1900
+		#elif MSVC_TOOLSET_VERSION == 140
 			#pragma comment(lib, "v14_smd_libjson.lib")
 		#endif
 	#else
-		#if _MSC_VER == 1800
+		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smt_libjson.lib")
-		#elif _MSC_VER == 1900
+		#elif MSVC_TOOLSET_VERSION == 140
 			#pragma comment(lib, "v14_smt_libjson.lib")
 		#endif
 	#endif
